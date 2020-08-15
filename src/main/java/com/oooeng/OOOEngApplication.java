1a51532061be5fe5e6d75a20217587c197ee2aaf
raman.chhabra <raman.chhabra@brightlifecare.com>
2020-08-15 22:31:26 +0530

    files added

--- src/main/java/com/oooeng/OOOEngApplication.java
+++ src/main/java/com/oooeng/OOOEngApplication.java
@@ -1,5 +1,14 @@
 package com.oooeng;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@SpringBootApplication
+@ComponentScan("com.oooeng.*")
 public class OOOEngApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OOOEngApplication.class, args);
+    }
+
 }
