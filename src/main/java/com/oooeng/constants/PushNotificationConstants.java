f2793ddb5f1c23bae846fea8613e4822fab33702
raman.chhabra <raman.chhabra@brightlifecare.com>
2020-08-23 12:30:54 +0530

    files added

--- src/main/java/com/oooeng/constants/PushNotificationConstants.java
+++ src/main/java/com/oooeng/constants/PushNotificationConstants.java
@@ -4,4 +4,9 @@ public interface PushNotificationConstants {
     String URL = "https://fcm.googleapis.com/fcm/send";
     String AUTH_KEY = "AAAAcbcWs7Y:APA91bHe24HzYAc3gRZV21bzSEyepvW4_IIsiL0EeNlNUo2dQJ5EJ5MmQjMm3PpkOqG0_IzqKO3SM5aZ6Zw17d2VIpHXEJtUfGnQ8e93apf0epAew74-Hx-MHFVlc3DrFJLv6RwHDAqx";
 
+    String TITLE = "Call from OOOEng";
+    String MESSAGE = "Please pick your call";
+    String IMAGE = "";
+    String ACTION_CALL_URL = "";
+
 }
--- src/main/java/com/oooeng/rest/communication/CommunicationController.java
+++ src/main/java/com/oooeng/rest/communication/CommunicationController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 
+import static com.oooeng.constants.PushNotificationConstants.*;
+
 @RestController
 @RequestMapping("/communication")
 public class CommunicationController {
@@ -31,23 +33,20 @@ public class CommunicationController {
     }
 
     @RequestMapping(value = "/sendAgoraNotification", method = RequestMethod.POST)
-    public void sendAgoraNotification(@RequestBody JSONObject jsonBody) throws Exception {
+    public void sendAgoraNotification(@RequestBody JSONObject jsonBody, @RequestHeader String platform) throws Exception {
         ArrayList list = (ArrayList) jsonBody.get("fcmIds");
         JSONArray fcmIds = new JSONArray();
         int length = list.size();
         for (int i = 0; i < length; i++) {
             fcmIds.add(list.get(i));
         }
-        String title = (String) jsonBody.get("title");
-        String message = (String) jsonBody.get("message");
+
         String accessToken = (String) jsonBody.get("accessToken");
         String channelName = (String) jsonBody.get("channelName");
         String type = (String) jsonBody.get("type");
-        String image = (String) jsonBody.get("image");
-        String url = (String) jsonBody.get("url");
-        int platform = 1;
 
-        service.sendNotification(title, message, accessToken, channelName, image, url, platform, type, fcmIds);
+
+        service.sendNotification(TITLE, MESSAGE, accessToken, channelName, IMAGE, ACTION_CALL_URL, Integer.parseInt(platform), type, fcmIds);
     }
 
 }
